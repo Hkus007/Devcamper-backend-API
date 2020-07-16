85aa9e7acafb97a04244f2c29703236194933753
Hemant Kumar Singh <hemant.kumar@impactanalytics.co>
2020-07-16 14:44:40 +0530

    performing crud operations

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,19 +1,73 @@
-exports.getBootcamps = (req, res) => {
-  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+const Bootcamp = require('../models/Bootcamp');
+
+exports.getBootcamp = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
-exports.getBootcamp = (req, res) => {
-  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+exports.getBootcamps = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
-exports.createBootcamp = (req, res) => {
-  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+exports.createBootcamp = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
-exports.updateBootcamps = (req, res) => {
-  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+exports.updateBootcamps = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
-exports.deleteBootcamps = (req, res) => {
-  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+exports.deleteBootcamps = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
